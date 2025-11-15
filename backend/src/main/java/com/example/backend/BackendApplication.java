7a0b05aa69f16f20b9bc336681f0eb5e3a968132
Kushal rathore <132640976+199904092023@users.noreply.github.com>
2025-11-15 18:51:07 +0530

    Testing Kushal..

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -9,6 +9,7 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 		System.out.println("Hi, Sparsh Garg");
+        System.out.println("Hi, Kushal Rathore");
 	}
 
 }
